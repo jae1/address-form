1aff74fc0035008b9ae9fe7f59b3fad56ce90623
jae1 <7904001@gmail.com>
2020-03-04 19:48:41 -0800

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,9 @@ mongoose.connect(dbConfig.db, {
 // Setting up port with express js
 const addressRoute = require('../backend/routes/address.route')
 const app = express()
-         .use(bodyParser.json())
-         .use(bodyParser.urlencoded({ extended: false }))
-         .use(cors()); 
+         .use(bodyParser.json(bodyParser.json({limit:'50mb'})))
+         .use(bodyParser.urlencoded({ extended: true, limit:'50mb' }))
+         .use(cors());
 
 // app.use(express.static(path.join(__dirname, 'dist/mean-stack-crud-app')));
 // app.use('/', express.static(path.join(__dirname, 'dist/mean-stack-crud-app')));app.use('/', express.static(path.join(__dirname, 'dist/mean-stack-crud-app')));
