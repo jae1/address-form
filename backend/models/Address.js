2a9fd786af17344d4454537be06d77fefff5a888
Heetae Yang <htyang90@gmail.com>
2020-03-04 23:53:12 -0800

    Added 'required' field on addr1, locale, region, and postal-code

--- backend/models/Address.js
+++ backend/models/Address.js
@@ -1,6 +1,40 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+let Address = new Schema({
+  country: String,
+  address1: {
+    type: String,
+    required: [true, 'Address required.'],
+    //match: "^(\w*\s*[\#\-\,\/\.\(\)\&]*)+$"
+  },
+
+  address2: String,
+  address3: String,
+  locale: {
+    type: String,
+    required: [true, 'Locale required.'],
+    //match: "^(\w*\s*[\#\-\,\/\.\(\)\&]*)+$"
+  },
+
+  region: {
+    type: String,
+    required: [true, 'Region required.'],
+    //match: "^(\w*\s*[\#\-\,\/\.\(\)\&]*)+$"
+  },
+
+  postalCode: {
+    type: String,
+    required: [true, 'Postal Code required.'],
+    //match: "^(\w*\s*[\#\-\,\/\.\(\)\&]*)+$"
+  }
+}, {
+  collection: 'addresses'
+});
+
+module.exports = mongoose.model('Address', Address);
+
+
 // Define collection and schema
 // let Address = new Schema({
 //       country,
@@ -45,17 +79,3 @@ const Schema = mongoose.Schema;
 // }, {
 //    collection: 'addresses'
 // });
-
-let Address = new Schema({
-   country: String,
-   address1: String,
-   address2: String,
-   address3: String,
-   locale: String,
-   region: String,
-   postalCode: String
-}, {
-collection: 'addresses'
-});
-
-module.exports = mongoose.model('Address', Address);
