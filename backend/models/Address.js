d52a1d493980fb30a22699b30ca8e5e9a3353dda
jae1 <7904001@gmail.com>
2020-03-04 22:16:00 -0800

    Update Address.js

--- backend/models/Address.js
+++ backend/models/Address.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const country = require('../models/Country');
 
 // Define collection and schema
 // let Address = new Schema({
