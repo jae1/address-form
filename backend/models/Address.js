a1a33f2f1f83786c9e4f263704d6197e2acd28fe
jae1 <7904001@gmail.com>
2020-03-10 17:55:51 -0700

    update

--- backend/models/Address.js
+++ backend/models/Address.js
@@ -5,29 +5,28 @@ let Address = new Schema({
   country: String,
   address1: {
     type: String,
-    required: [true, 'Address required.'],
-    match: [/^(\w*\s*[\#\-\,\/\.\(\)\&]*)+$/, 'is invalid']
+    required: [true, 'Address required.']
+    // match: [/^(\w*\s*[\#\-\,\/\.\(\)\&]*)+$/, 'is invalid']
   },
 
   address2: String,
   address3: String,
   locale: {
-    type: String,
-    required: [true, 'Locale required.'],
-    match: [/^[A-z]+$/, 'is invalid']
+    type: String
+    // required: [true, 'Locale required.'],
+    // match: [/^[A-z]+$/, 'is invalid']
   },
 
   region: {
-    type: String,
-    required: [true, 'Region required.'],
-    match: [/^[A-z]+$/, 'is invalid']
+    type: String
+    // required: [true, 'Region required.'],
+    // match: [/^[A-z]+$/, 'is invalid']
   },
 
   postalCode: {
-    type: String,
-    required: [true, 'Postal Code required.'],
-    match: [/^\d{5}$/, 'is invalid']
-
+    type: String
+    // required: [true, 'Postal Code required.'],
+    // match: [/^\d{5}$/, 'is invalid']
   }
 }, {
   collection: 'addresses'
